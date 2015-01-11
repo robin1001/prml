13c5c251ec9cd31f5c3986d106aa005f04f2ec28
robin1001 <811364747@qq.com>
2015-01-11 21:24:51 +0800

    depressed

--- linear_classify/sigmod_regression.py
+++ linear_classify/sigmod_regression.py
@@ -1,24 +1,31 @@
 from pylab import *
 
 def sigmod(x):
-    return 1/1+exp(-x) 
+    return 1.0 / (1 + exp(-x))
 
-data = np.loadtxt('data.txt')
+data = mat(np.loadtxt('data.txt'))
 num = data.shape[0]
 print num
-x1, x2, t = data[:,0].T, data[:,1].T, data[:,2].T
+x1, x2, t = data[:,0], data[:,1], data[:,2]
 for i in range(num):
     if t[i] == 0:
         plot(x1[i], x2[i], 'ro')
     else:
         plot(x1[i], x2[i], 'bo')
 
-#x = hstack((x1, x2, ones((num, 1))))
-#w = ones((3, 1))
-#epock = 1000
-#for i in range(epock):
-#   y = sigmod(dot(x, w))
-#   dw = dot((y - t).T, x)
-#   w = w + dw
+x = hstack((x1, x2, ones((num, 1))))
+w = ones((3, 1))
+epock = 1000
+alpha = 0.001
+for i in range(epock):
+   y = sigmod(x * w)
+   dw = x.T * (y - t) 
+   w = w + multiply(alpha, dw)
 
+print w
+y = mat(zeros((num, 1)))
+for i in range(num):
+	y[i] = -(w[0] * x1[i] + w[2]) / w[1]
+yy = hstack((x2, y))
+print yy
 show()
