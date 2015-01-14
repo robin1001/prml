cec2baa6c41c1eec0d661007eeeace6d168eddf1
robin1001 <811364747@qq.com>
2015-01-14 12:51:06 +0800

    nn version 0.01

--- nn/test_nn.py
+++ nn/test_nn.py
@@ -1,8 +1,11 @@
 from numpy import *
 from nn import *
+
 data = mat(loadtxt('data.txt'))
-x, y = data[:, 0:2], data[:, -1]
+train_x, train_y = data[:800, 0:2], data[:800, -1]
+test_x, test_y = data[800:, 0:2], data[800:, -1]
 
 
-net = NN(x, y, [2, 3, 1], sigmoid, linear)
-net.start()
+net = NN(train_x, train_y, [2, 3, 1], sigmoid, linear)
+net.train()
+net.test_regression(test_x, test_y)
