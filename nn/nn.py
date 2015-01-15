bd066c0833805027328115cef90697d28a55227e
robin1001 <811364747@qq.com>
2015-01-15 21:56:14 +0800

    version 3

--- nn/nn.py
+++ nn/nn.py
@@ -1,78 +1,81 @@
 from numpy import *
 from numpy.random import *
 
-def sigmoid(msg, x):
-    if msg == 'fp': return 1.0 / (1.0 + exp(-x))
-    elif msg == 'bp': return multiply(x, 1 - x)
-    elif msg == 'loss': pass        
-        
-def tanh(msg, x):
-    if msg == 'fp': return (exp(x) - exp(-x)) / (exp(x) + exp(-x))
-    elif msg == 'bp': return 1 - multiply(x, x)
+# author: robin1001 date: 2015-01-15
 
-def linear(msg, x):
-    if msg == 'fp': return x
-    elif msg == 'loss': pass
-    elif msg == 'bp': return -(x[0] - x[1])
-
-def softmax(msg, x):
-    if msg == 'fp':  pass
-    elif msg == 'bp': pass
-    elif msg == 'loss': pass
+def sigmoid(x):
+    return 1 / (1 + exp(-x))
+def tanh(x):
+    return (exp(x)-exp(-x)) / (exp(x)+exp(-x))
 
 class NN(object):
-    def __init__(self, x, y, layer, activefn=sigmoid, outfn=sigmoid):
+    def __init__(self, x, y, layer):
         ''' x: input 
             y: output 
             layer: layer structrue, eg: [400 100 10]
             outfn: output active funciton, eg: sigmoid, softmax, linear
             activefn: active function, eg: sigmoid, 
         '''
-        self.activefn = activefn 
-        self.outfn = outfn
+        self.activefn = 'sigmoid' 
+        self.outfn = 'linear'
         self.x = mat(x)
         self.y = mat(y)
         self.layer = layer
         self.n = len(layer) #num of layer
         self.epoch = 1000
         self.learning_rate = 0.5
+        self.batch_size = self.x.shape[0] #default 1 batch
         self.setup()
 
     def setup(self):
         '''setup the neural network, init w'''
         self.w = [None]*(self.n - 1)
         self.a = [None]*self.n
-        for i in range(self.n - 1): 
-            self.w[i] = mat(rand(self.layer[i+1], self.layer[i] + 1)) # 1 bias
+        for i in range(self.n - 1):
+            n1, n2 = self.layer[i], self.layer[i+1]
+            self.w[i] = mat(rand(n2, n1 + 1) - 0.5) / (n1 + n2)# 1 bias
+            #self.w[i] = mat(rand(n2, n1 + 1)) # 1 bias
 
     def fp(self, x, y):
         '''forward propogation'''
         m = x.shape[0]
         self.a[0] = hstack((x, ones((m, 1))))
         for i in range(1, self.n - 1):
-            self.a[i] = self.activefn('fp', self.a[i-1] * self.w[i-1].T)
+            if self.activefn == 'sigmoid':
+                self.a[i] = sigmoid(self.a[i-1] * self.w[i-1].T)
+            else:# tanh
+                self.a[i] = tanh(self.a[i-1] * self.w[i-1].T)
             #add one bias 
             self.a[i] = hstack((self.a[i], ones((m, 1))))
+        a = self.a[-2] * self.w[-1].T
         #switch output function
-        self.a[-1] = self.outfn('fp', self.a[-2] * self.w[-1].T)
-        e = y - self.a[-1]
-        loss = (e.T * e) / m
-        return loss
+        if self.outfn == 'linear':
+            self.a[-1] = a
+            loss = (y - self.a[-1]).T * (y - self.a[-1]) / m
+        elif self.outfn == 'sigmoid':
+            self.a[-1] = sigmoid(a)
+            loss = -sum(multiply(y, self.a[-1]) + multiply(1-y, 1-self.a[-1])) / m
+        elif self.outfn == 'softmax':
+            self.a[-1] = exp(a) / sum(exp(a), 1)
+            loss = -sum(multiply(y, log(self.a[-1]))) 
+        return loss, self.a[-1]
         
-    def ebp(self):
+    def ebp(self, x, y):
         '''error backpropogation
            bishop prml neural network fomulation(5.50 - 5.56)
            dw: difference of w
            d: delta 
            h: difference of acitve function, eg:d(s(a)) = s(a) * (1 - s(a))
+           sigmoid use cross entropy loss, linear output square loss
         '''
         dw, d = [None]*(self.n-1), [None]*self.n
         #switch output funciton
-        d[-1] = self.outfn('bp',(self.y, self.a[-1]))
-        m = self.y.shape[0]
+        d[-1] = -(y - self.a[-1])
+        m = y.shape[0]
         for i in range(self.n - 2, 0, -1):
             #multiply fucntion: numpy dot multiply fucntion, like .* in matlab
-            h = self.activefn('bp', self.a[i])
+            if self.activefn == 'sigmoid': h = multiply(self.a[i], self.a[i])
+            else: h = 1 - multiply(self.a[i], self.a[i])
             if i+1 == self.n - 1: #last layer
                 d[i] = multiply(h, d[i+1] * self.w[i])
             else:
@@ -83,7 +86,7 @@ class NN(object):
                 dw[i] = (d[i+1].T * self.a[i]) / m
             else:
                 dw[i] = (d[i+1][:,:-1].T * self.a[i]) / m
-            #print i, self.check_gradient(i, dw[i])
+            #print i, self.check_gradient(i, dw[i], x, y)
 
         #gradient descent
         for i in range(0, self.n - 1):
@@ -91,12 +94,22 @@ class NN(object):
     
                
     def train(self):
+        m, n = self.x.shape
+        num_batch, left = m / self.batch_size, m % self.batch_size
+        if left != 0: num_batch += 1
         for i in range(self.epoch):
-            loss = self.fp(self.x, self.y)
-            self.ebp()
-            print "loss", loss
+            start, total_loss = 0, 0
+            for j in range(num_batch):
+                end = start + self.batch_size
+                if end > m: end = m
+                loss, p = self.fp(self.x[start:end], self.y[start:end])
+                self.ebp(self.x[start:end], self.y[start:end])
+                print "epoch:%d minibatch:%d loss:%f " % (i, j, loss)
+                total_loss += loss
+                start += self.batch_size
+            print "epoch:%d total_loss:%f\n" % (i, total_loss)
 
-    def check_gradient(self, k, dw):
+    def check_gradient(self, k, dw, x, y):
         ''' check gradient of k th w 
             coursera standord ng machine learning chapter 9-5'''
         eps = 0.0001
@@ -106,20 +119,14 @@ class NN(object):
             for j in range(n):
                 w = self.w[k][i,j]
                 self.w[k][i,j] = w - eps 
-                l1 = self.fp(self.x, self.y)
+                l1, p = self.fp(x, y)
                 self.w[k][i,j] = w + eps 
-                l2 = self.fp(self.x, self.y) 
+                l2, p = self.fp(x, y) 
                 d[i, j] = (l2 - l1) / (2 * eps)
                 self.w[k][i,j] = w #keep self.w invariant  
         err = d - dw
         return sum(multiply(err, err)) 
 
-    def test_regression(self, x, y):
-        self.fp(mat(x), mat(y))
-        #com = hstack((y, self.a[-1]))
-        return self.a[-1]
-
-    def test_classify(self, x, y):
-        self.fp(mat(x), mat(y))
-
-
+    def test(self, x, y):
+        loss, p = self.fp(mat(x), mat(y))
+        return loss, p
--- nn/test_nn_regression.py
+++ nn/test_nn_regression.py
@@ -1,16 +1,20 @@
 from pylab import *
 from nn import *
 
-data = mat(loadtxt('data_regression_1000.txt'))
+data = mat(loadtxt('addnum.txt'))
 train_x, train_y = data[:800, 0:2], data[:800, -1]
 test_x, test_y = data[800:, 0:2], data[800:, -1]
 
 
-net = NN(train_x, train_y, [2,  10, 1], sigmoid, linear)
+net = NN(train_x, train_y, [2, 3, 1])
+net.activefn = 'sigmoid'  #learn rate 1.0
+#net.activefn = 'tanh'
+net.outfn = 'linear'
 net.epoch = 1000
-net.learning_rate = 0.5
+net.learning_rate = 1.0
+net.batch_size = 10
 
 net.train()
-y = net.test_regression(test_x, test_y)
-plot(y - test_y)
+loss, y = net.test(test_x, test_y)
+plot(abs(y - test_y))
 show()
