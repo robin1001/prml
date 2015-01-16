41098b0b832e5515de8b1f6d6f856f7b8b011c8c
robin1001 <811364747@qq.com>
2015-01-16 18:05:23 +0800

    version 10.0

--- nn/test_nn_multi_classify.py
+++ nn/test_nn_multi_classify.py
@@ -18,11 +18,11 @@ img = train_x[0,:].copy().reshape(28,28)
 imshow(img, cmap=plt.cm.gray)
 
 net = NN(train_x, train_y, [784, 100, 10])
-#net.activefn = 'sigmoid'  #learn rate 1.0
-net.activefn = 'tanh'
-#net.outfn = 'softmax'
-net.outfn = 'sigmoid'
-net.epoch = 1
+net.activefn = 'sigmoid'  #learn rate 1.0
+#net.activefn = 'tanh'
+net.outfn = 'softmax'
+#net.outfn = 'sigmoid'
+net.epoch = 10
 net.batch_size = 100
 net.learning_rate = 1.0
 #
