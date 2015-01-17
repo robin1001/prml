50e5ae85480e6be51a7778fe42fb788165e895ef
robin1001 <811364747@qq.com>
2015-01-17 11:58:11 +0800

    version 10.2

--- nn/test_nn_multi_classify.py
+++ nn/test_nn_multi_classify.py
@@ -9,26 +9,26 @@ train_x, train_y = data['train_x'][:train_num], data['train_y'][:train_num]
 test_x, test_y = data['test_x'][:test_num], data['test_y'][:test_num]
 train_x, train_y = mat(train_x) / 255.0, mat(train_y)
 test_x, test_y = mat(test_x) / 255.0, mat(test_y)
-mu, sigma = mean(train_x, 0), var(train_x, 0)
-sigma = maximum(sigma, 2.2204e-10)
-train_x = (train_x - mu) / sigma
-test_x = (test_x - mu) / sigma
-
-img = train_x[0,:].copy().reshape(28,28)
-imshow(img, cmap=plt.cm.gray)
+#mu, sigma = mean(train_x, 0), var(train_x, 0)
+#sigma = maximum(sigma, 2.2204e-10)
+#train_x = (train_x - mu) / sigma
+#test_x = (test_x - mu) / sigma
 
 net = NN(train_x, train_y, [784, 100, 10])
 net.activefn = 'sigmoid'  #learn rate 1.0
 #net.activefn = 'tanh'
-net.outfn = 'softmax'
-#net.outfn = 'sigmoid'
-net.epoch = 20
-net.batch_size = 1000
-net.learning_rate = 2.0
-#
+#net.outfn = 'softmax'
+net.outfn = 'sigmoid'
+net.epoch = 25
+net.batch_size = 100
+net.learning_rate = 1.2
+
 net.train()
 loss, y = net.test(test_x, test_y)
 in1 = argmax(test_y, 1)
 in2 = argmax(y, 1)
 print mean(in1 == in2)
+
+#img = train_x[0,:].copy().reshape(28,28)
+#imshow(img, cmap=plt.cm.gray)
 #show()
